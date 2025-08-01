4883549ff0bfca2dc481ef9ce7445cf540db1e56
Sulthon Farel Abdur Qois <149635659+elmooow@users.noreply.github.com>
2025-08-01 22:07:55 +0700

    koneksi database

---
+++ db_diskon.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE db_diskon;
+USE db_diskon;
+
+CREATE TABLE barang (
+    id_barang INT AUTO AUTO_INCREMENT PRIMARY KEY,
+    nama_barang VARCHAR(100),
+    harga INT NOT NULL,
+);
+
+CREATE TABLE transaksi (
+    id_transaksi INT AUTO_INCREMENT PRIMARY KEY,
+    id_barang INT,
+    diskon INT,
+    harga_setelah_diskon INT,
+    tanggal DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_barang) REFERENCES barang(id_barang)
+);
